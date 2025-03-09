f02e542ce371ad4044ba7a3fbc8a7726bc489391
gautamxv <gk896523@gmail.com>
2025-03-09 19:33:10 +0530

    creating function of js

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function calculate() {
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
+
+
+function backspace() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1); 
+    
+}
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if ('0123456789+-*/'.includes(key)) {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        event.preventDefault(); // Prevent the default backspace action
+        backspace();
+    } else if (key === 'c' || key === 'C') {
+        clearDisplay();
+    }
+});
